91b2a390e5ba58ed3b40172c3e3b3f919df9c21b
Manuel Alejandro Garisto Zuna <garistozunamanuel@gmail.com>
2020-11-30 20:07:32 -0400

    feat: Add the database models and connection

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ import babel
 from flask import Flask, render_template, request, Response, flash, redirect, url_for
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 import logging
 from logging import Formatter, FileHandler
 from flask_wtf import Form
@@ -21,42 +22,81 @@ moment = Moment(app)
 app.config.from_object('config')
 db = SQLAlchemy(app)
 
-# TODO: connect to a local postgresql database
-
+migrate = Migrate(app,db)
 #----------------------------------------------------------------------------#
 # Models.
 #----------------------------------------------------------------------------#
 
+# Relationships
+# Venue - Show: one to Many, bidirectional
+# Artist - Show: one to Many, bidirectional
 class Venue(db.Model):
     __tablename__ = 'Venue'
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String, nullable=False)
+    city = db.Column(db.String(120), nullable=False)
+    state = db.Column(db.String(120),nullable=False)
     address = db.Column(db.String(120))
     phone = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
+    genres = db.Column(db.ARRAY(db.String))
+    website = db.Column(db.String)
+    seeking_talent = db.Column(db.Boolean)
+    seeking_description = db.Column(db.String)
 
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
+    shows = db.relationship("Show", back_populates='venue', lazy=True)
+
+    #Prints an object as a string
+    def __repr__(self):
+      return str({
+        'id':self.id,
+        'name':self.name,
+        'city':self.city,
+        'state':self.state,
+        'address':self.address,
+        'phone':self.phone,
+        'image_link':self.image_link,
+        'facebook_link':self.facebook_link,
+        'genres':self.genres,
+        'website':self.website,
+        'seeking_talent':self.seeking_talent,
+        'seeking_description':self.seeking_description
+      })
 
 class Artist(db.Model):
     __tablename__ = 'Artist'
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String, nullable=False)
+    city = db.Column(db.String(120), nullable=False)
+    state = db.Column(db.String(120), nullable=False)
     phone = db.Column(db.String(120))
-    genres = db.Column(db.String(120))
+    genres = db.Column(db.ARRAY(db.String))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
+    seeking_venue = db.Column(db.Boolean)
+    seeking_description = db.Column(db.String)
+    website = db.Column(db.String)
 
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
+    shows = db.relationship("Show", back_populates ='artist')
 
-# TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
-
+    # Prints the object as a string 
+    def __repr__(self):
+      return str({
+        'id':self.id,
+        'name':self.name,
+        'city':self.city,
+        'state':self.state,
+        'phone':self.phone,
+        'image_link':self.image_link,
+        'facebook_link':self.facebook_link,
+        'genres':self.genres,
+        'website':self.website,
+        'seeking_venue':self.seeking_venue,
+        'seeking_description':self.seeking_description
+      })
 #----------------------------------------------------------------------------#
 # Filters.
 #----------------------------------------------------------------------------#
